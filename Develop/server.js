08456111cd8933880af2515a8df8a31fffeed003
Jay Idrees <idreesjay@gmail.com>
2020-11-21 16:35:15 -0500

    setup gitignore before installing node packages

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,11 @@ const PORT=process.env.PORT|| 8080;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static(path.join(_dirname,'public')));
 
+require ('./routes/api-routes')(app)
+require ('./routes/html-routes')(app)
 
-
-
+app.listen(PORT, function(){
+    console.log('The Surver is now running at http://localhost:'+PORT)
+})
